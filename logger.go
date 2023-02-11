c894de7f9e9d5d74ee4f964237992c3b0e96d9f7
Mikhail Bigun <bigun.md@gmail.com>
2023-02-11 14:54:48 +0300

    add logger

---
+++ logger.go
@@ -0,0 +1,46 @@
+package fiberlogrus
+
+import (
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	log "github.com/sirupsen/logrus"
+)
+
+// getLogrusFields calls FuncTag funcnions on matching keys
+func getLogrusFields(ftm map[string]FuncTag, c *fiber.Ctx, d *data) log.Fields {
+	f := make(log.Fields)
+	for k, ft := range ftm {
+		f[k] = ft(c, d)
+	}
+	return f
+}
+
+// New creates a new middleware handler
+func New(config ...Config) fiber.Handler {
+	var cfg Config
+	if len(config) == 0 {
+		cfg = ConfigDefault
+	} else {
+		cfg = config[0]
+	}
+	d := new(data)
+	// Set PID once
+	d.pid = os.Getpid()
+	ftm := getFuncTagMap(cfg, d)
+
+	return func(c *fiber.Ctx) error {
+		d.start = time.Now()
+		c.Next()
+		d.end = time.Now()
+		switch cfg.Logger {
+		case nil:
+			log.WithFields(getLogrusFields(ftm, c, d)).Info()
+		default:
+			cfg.Logger.WithFields(getLogrusFields(ftm, c, d)).Info()
+		}
+
+		return nil
+	}
+}
