04ffd838d02dd67f7dbf5f059d87c0baa85af29b
Qinglong Wang <35256908+Ginakira@users.noreply.github.com>
2023-07-18 15:02:40 +0800

    fix: prevent to ignore error returned from the next middleware

--- logger.go
+++ logger.go
@@ -32,7 +32,7 @@ func New(config ...Config) fiber.Handler {
 
 	return func(c *fiber.Ctx) error {
 		d.start = time.Now()
-		c.Next()
+		err := c.Next()
 		d.end = time.Now()
 		switch cfg.Logger {
 		case nil:
@@ -41,6 +41,6 @@ func New(config ...Config) fiber.Handler {
 			cfg.Logger.WithFields(getLogrusFields(ftm, c, d)).Info()
 		}
 
-		return nil
+		return err
 	}
 }
